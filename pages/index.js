e5a2df8a39bcdb657159b19263b9140d3ca5d2e0
AdryanS <adryx7000@gmail.com>
2021-02-03 15:45:48 -0300

    pao

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import React, { useState } from 'react'
 export default function Home() {
+
+  let [count, setCount] = useState(0)
 
   return (
     <div >
-      <h1>hello world</h1>
+      <button onClick={() => setCount(count + 1)}>click here</button>
+      <h1>contador {count}</h1>
     </div>
   )
 }
